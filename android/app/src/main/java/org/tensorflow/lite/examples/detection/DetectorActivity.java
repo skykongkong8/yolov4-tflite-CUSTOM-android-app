fe1b30e46c72f9fb58b076976b643989163c9b72
SungSikKong <kssjustin98@gmail.com>
2021-08-26 11:21:15 +0900

    Update to 421 model

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -54,7 +54,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
 
     private static final int TF_OD_API_INPUT_SIZE = 416;
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
-    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-420.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-421.tflite";
 
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/_darknet.labels";
 
